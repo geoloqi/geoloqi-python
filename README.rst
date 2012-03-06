81c4539eb852e29cb56f1e28e3668b052bbf936c
Tristan Waddington <tristan.waddington@gmail.com>
2012-03-05 16:47:42 -0800

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,26 @@
+Geoloqi is a powerful platform for real-time location, messaging, and
+analytics. For more information visit `Geoloqi.com`_.
+
+Getting Started
+===============
+...
+
+Examples
+========
+...
+
+Contributing
+============
+Please fork the project on `GitHub`_ and send us a pull request! If you have
+a problem, please file an issue! We'll respond as soon as possible.
+
+There are many other  ways to `get support`_ from the Geoloqi `developers site`_.
+
+License
+=======
+See LICENSE.
+
+.. _Geoloqi.com: https://www.geoloqi.com/
+.. _get support: https://developers.geoloqi.com/support/
+.. _developers site: https://developers.geoloqi.com/
+.. _GitHub: https://github.com/twaddington/geoloqi-python/
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+readme = open('README.rst')
+
+setup(
+    name='geoloqi-python',
+    version='1.0.0',
+    description='A powerful platform for mobile location, messaging and analytics.',
+    long_description=readme.read(),
+    author='Tristan Waddington',
+    author_email='tristan.waddington@gmail.com',
+    url='https://github.com/twaddington/geoloqi-python',
+    packages=['geoloqi',],
+    classifiers=[
+        'Development Status :: 4 - Beta', # 4 Beta, 5 Production/Stable
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
