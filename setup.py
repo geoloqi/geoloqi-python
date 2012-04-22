1dc07e43c6a29c8063b4f94f2ff6d446e296d4e9
Wraithan <xwraithanx@gmail.com>
2012-04-22 16:39:10 -0700

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # Get the version number
 execfile('geoloqi/version.py')
